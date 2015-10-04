ee88930db21b861159cf63ead4197330cf4440a3
Patrick Hart <thrifus@gmail.com>
2015-10-04 13:44:56 -0500

    Update to 1.0.7

--- Formula/tortle.rb
+++ Formula/tortle.rb
@@ -1,7 +1,7 @@
 class Tortle < Formula
   desc "Tiny utility for easily enabling or disabling Tor"
   homepage "https://thrifus.github.io/Tortle"
-  url "https://github.com/thrifus/Tortle/archive/1.0.6.tar.gz"
+  url "https://github.com/thrifus/Tortle/archive/1.0.7.tar.gz"
   sha256 "d0825861916f2c5a79728e5d497499c17b96db1c1b77c015a0068a28b61b9462"
 
   depends_on "tor"
