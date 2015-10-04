ec51fb56f13e06a30c387be3fae6974291b1eed9
Patrick Hart <thrifus@gmail.com>
2015-10-04 13:46:24 -0500

    Update to 1.0.8
    When will I actually get this crap right?

--- Formula/tortle.rb
+++ Formula/tortle.rb
@@ -1,8 +1,8 @@
 class Tortle < Formula
   desc "Tiny utility for easily enabling or disabling Tor"
   homepage "https://thrifus.github.io/Tortle"
-  url "https://github.com/thrifus/Tortle/archive/1.0.7.tar.gz"
-  sha256 "d0825861916f2c5a79728e5d497499c17b96db1c1b77c015a0068a28b61b9462"
+  url "https://github.com/thrifus/Tortle/archive/1.0.8.tar.gz"
+  sha256 "31eac6e2c3767bbf5281b907eb04f9e8b82bb2d707adfd4d43abd1b6cadf5072"
 
   depends_on "tor"
 
